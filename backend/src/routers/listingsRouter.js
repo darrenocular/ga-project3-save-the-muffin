adbdb03eabe0800fdffc56ce5e1655e82fa29170
Darren Leong <darrenljj07@hotmail.com>
2024-04-02 17:52:58 +0800

    Add listingsValidator.js

--- backend/src/routers/listingsRouter.js
+++ backend/src/routers/listingsRouter.js
@@ -10,11 +10,21 @@ const {
   getListingsByMerchantId,
   getEnum,
 } = require("../controllers/listingsController");
+const {
+  validateNewListingInput,
+  validateUpdateListingInput,
+} = require("../validators/listingsValidator");
+const { errorCheck } = require("../validators/errorCheck");
 
 router.get("/listings", getAllListings); // get all listings
 router.get("/listings/seed", seedListings); // seed listings
-router.put("/listings", addNewListing); // add a new listing
-router.patch("/listings", updateListingById); // update a particular listing
+router.put("/listings", validateNewListingInput, errorCheck, addNewListing); // add a new listing
+router.patch(
+  "/listings",
+  validateUpdateListingInput,
+  errorCheck,
+  updateListingById
+); // update a particular listing
 router.delete("/listings", deleteListingById); // delete a listing
 router.post("/listings", getListingById); // get a listing by listing id
 router.post("/listings/merchant", getListingsByMerchantId); // get listings by merchant id
