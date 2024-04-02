5ac3e259ad02e2e65d8e956916bdc2890c57756d
Darren Leong <darrenljj07@hotmail.com>
2024-04-02 14:41:31 +0800

    Add seedCart and getCartByUserId controller functions

--- backend/src/controllers/cartItemsController.js
+++ backend/src/controllers/cartItemsController.js
@@ -1,12 +1,74 @@
 const Cart = require("../models/CartItem");
 
-const getCartByUserId = () => {};
+const seedCart = async (req, res) => {
+  try {
+    await Cart.deleteMany();
 
-const updateCartItemByUserId = () => {};
+    await Cart.create([
+      {
+        user: "660b7382f4e248c36c993f29",
+        listing: "660ba0d63ca981e1aaa6626c",
+        cartQuantity: 5,
+      },
+      {
+        user: "660b7382f4e248c36c993f29",
+        listing: "660ba0d53ca981e1aaa6626a",
+        cartQuantity: 4,
+      },
+      {
+        user: "660b73a2f4e248c36c993f2b",
+        listing: "660ba0d53ca981e1aaa6626a",
+        cartQuantity: 5,
+      },
+      {
+        user: "660b73a2f4e248c36c993f2b",
+        listing: "660ba0d53ca981e1aaa6626b",
+        cartQuantity: 5,
+      },
+    ]);
 
-const deleteCartItemByUserId = () => {};
+    res.json({ status: "ok", msg: "seed success" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "seed fail" });
+  }
+};
+
+const getCartByUserId = async (req, res) => {
+  try {
+    const cart = await Cart.find({ user: req.body.user })
+      .populate("user listing")
+      .exec();
+
+    res.json(cart);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "failed to get cart" });
+  }
+};
+
+const updateCartItemByUserId = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(400)
+      .json({ status: "error", msg: "failed to update cart item" });
+  }
+};
+
+const deleteCartItemByUserId = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(400)
+      .json({ status: "error", msg: "failed to delete cart item" });
+  }
+};
 
 module.exports = {
+  seedCart,
   getCartByUserId,
   updateCartItemByUserId,
   deleteCartItemByUserId,
--- backend/src/routers/cartItemsRouter.js
+++ backend/src/routers/cartItemsRouter.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
+  seedCart,
   getCartByUserId,
   updateCartItemByUserId,
   deleteCartItemByUserId,
 } = require("../controllers/cartItemsController");
 
+router.get("/cart/seed", seedCart); // seed cart by user id
 router.post("/cart", getCartByUserId); // get user's cart items
 router.patch("/cart", updateCartItemByUserId); // update user's cart item
 router.delete("/cart", deleteCartItemByUserId); // delete user's cart item
