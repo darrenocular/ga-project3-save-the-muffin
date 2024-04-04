b4c03f8c1fd96adf16d2732d584bb8ec8924c472
Darren Leong <darrenljj07@hotmail.com>
2024-04-04 18:56:30 +0800

    Initialize Orders.jsx

--- frontend/project3/src/pages/Orders.jsx
+++ frontend/project3/src/pages/Orders.jsx
@@ -1,13 +1,47 @@
-import React from "react";
-import styles from "./styles/Orders.module.css";
+import React, { useContext, useEffect, useState } from "react";
 import OrderItem from "../components/OrderItem";
+import useFetch from "../hooks/useFetch";
+import AppContext from "../context/AppContext";
 
 const Orders = () => {
+  const fetchData = useFetch();
+  const appCtx = useContext(AppContext);
+  const [orders, setOrders] = useState([]);
+
+  const getOrders = async () => {
+    try {
+      const res = await fetchData(
+        "/api/orders",
+        "POST",
+        { id: appCtx.id },
+        appCtx.accessToken
+      );
+
+      if (res.ok) {
+        setOrders(res.data);
+      }
+    } catch (error) {
+      appCtx.setErrorMessage(res.data);
+      appCtx.isError(true);
+    }
+  };
+
+  // Get orders on mount
+  useEffect(() => {
+    getOrders();
+  }, []);
+
   return (
-    <>
-      <div>Orders</div>
-      <OrderItem />
-    </>
+    <div className="mx-auto w-90 px-4 py-4 flex flex-col">
+      <h2 className="text-xl font-bold tracking-tight text-indigo-900 mx-auto">
+        Your Orders
+      </h2>
+      <div className="w-full">
+        {orders.map((order, idx) => {
+          return <OrderItem order={order} key={idx} />;
+        })}
+      </div>
+    </div>
   );
 };
 
