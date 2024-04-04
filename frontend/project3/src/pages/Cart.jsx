8fb1268d85101a30a8c8950ad0570a61e72b8883
Darren Leong <darrenljj07@hotmail.com>
2024-04-04 18:56:17 +0800

    Add handleClearCart function

--- frontend/project3/src/pages/Cart.jsx
+++ frontend/project3/src/pages/Cart.jsx
@@ -30,6 +30,26 @@ const Cart = () => {
 
   const handleCheckOut = async () => {};
 
+  const handleClearCart = async () => {
+    try {
+      const res = await fetchData(
+        "/api/cart/all",
+        "DELETE",
+        { user: appCtx.id },
+        appCtx.accessToken
+      );
+
+      if (res.ok) {
+        getCart();
+      } else {
+        throw new Error(res.data);
+      }
+    } catch (error) {
+      appCtx.setErrorMessage(error.message);
+      appCtx.setIsError(true);
+    }
+  };
+
   // Get cart by user id on mount
   useEffect(() => {
     getCart();
@@ -38,20 +58,29 @@ const Cart = () => {
   return (
     <div className="mx-auto w-90 px-4 py-4 flex flex-col">
       <h2 className="text-xl font-bold tracking-tight text-indigo-900 mx-auto">
-        Cart
+        Your Cart
       </h2>
       <div className="w-full">
         {cart.map((cartItem, idx) => {
           return <CartItem cartItem={cartItem} key={idx} getCart={getCart} />;
         })}
       </div>
-      <button
-        type="button"
-        className="w-1/6 mx-auto rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500"
-        onClick={handleCheckOut}
-      >
-        Check out
-      </button>
+      <div className="flex justify-center mt-4">
+        <button
+          type="button"
+          className="w-1/6 rounded-md bg-amber-600 px-3 py-2 text-sm font-semibold text-white shadow-sm mr-2 hover:bg-amber-500"
+          onClick={handleClearCart}
+        >
+          Clear cart
+        </button>
+        <button
+          type="button"
+          className="w-1/6 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500"
+          onClick={handleCheckOut}
+        >
+          Check out
+        </button>
+      </div>
     </div>
   );
 };
