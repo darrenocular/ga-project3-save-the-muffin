9e006358c938309c6407be4a7b08990bce3f01a7
Darren Leong <darrenljj07@hotmail.com>
2024-04-05 14:40:42 +0800

    Create Orders.jsx view and refactor code into ActiveOrderItem.jsx and PastOrderItem.jsx

---
+++ frontend/project3/src/components/ActiveOrderItem.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const OrderItem = (props) => {
+  return (
+    <>
+      <div className="flex justify-between pr-4 rounded shadow-md my-4">
+        <div className="flex min-w-0 gap-x-4">
+          <img
+            className="h-36 w-36 flex-none bg-gray-50 object-cover rounded-l"
+            src="https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <div className="flex flex-col justify-between py-4">
+            <div>
+              <p className="text-xl font-semibold leading-6 text-indigo-900 flex">
+                {props.order.listing.name}{" "}
+                <span className="text-xs bg-indigo-700 text-white rounded py-1 px-2 ml-2 border-none">
+                  {props.order.listing.category.charAt(0).toUpperCase() +
+                    props.order.listing.category.slice(1)}
+                </span>
+              </p>
+              <p className="mt-1 truncate text-sm font-medium leading-5 text-indigo-700">
+                {props.order.merchant.merchantDetails.name} (
+                {props.order.merchant.merchantDetails.address})
+              </p>
+            </div>
+            <div>
+              <p className="text-sm text-indigo-700">
+                Collection:{" "}
+                <span className="text-sm text-indigo-700">
+                  {new Date(props.order.listing.collectionDate).toLocaleString(
+                    "en-SG"
+                  )}
+                </span>
+              </p>
+              <p className="text-sm text-indigo-700">
+                Order #{props.order._id}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col py-4 pr-2 w-1/6">
+          <p className="text-sm leading-6 text-indigo-900 font-semibold">
+            Quantity ordered: {props.order.purchaseQuantity}
+          </p>
+          <p className="text-sm leading-6 text-indigo-900 font-semibold">
+            Unit price: S${props.order.listing.discountedPrice.toFixed(2)}
+          </p>
+          <p className="text-sm leading-6 text-indigo-900 font-semibold">
+            Total price: S${props.order.totalPrice}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OrderItem;
--- frontend/project3/src/pages/Orders.jsx
+++ frontend/project3/src/pages/Orders.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
-import OrderItem from "../components/OrderItem";
+import ActiveOrderItem from "../components/ActiveOrderItem";
+import PastOrderItem from "../components/PastOrderItem";
 import useFetch from "../hooks/useFetch";
 import AppContext from "../context/AppContext";
 
@@ -7,6 +8,9 @@ const Orders = () => {
   const fetchData = useFetch();
   const appCtx = useContext(AppContext);
   const [orders, setOrders] = useState([]);
+  const [activeOrders, setActiveOrders] = useState([]);
+  const [pastOrders, setPastOrders] = useState([]);
+  const [showHistory, setShowHistory] = useState(false);
 
   const getOrders = async () => {
     try {
@@ -31,15 +35,53 @@ const Orders = () => {
     getOrders();
   }, []);
 
+  // Set active and past orders on change to orders
+  useEffect(() => {
+    setActiveOrders(orders.filter((order) => order.isCollected === false));
+    setPastOrders(orders.filter((order) => order.isCollected === true));
+  }, [orders]);
+
   return (
     <div className="mx-auto w-90 px-4 py-4 flex flex-col">
       <h2 className="text-xl font-bold tracking-tight text-indigo-900 mx-auto">
         Your Orders
       </h2>
+      <p className="text-sm font-light text-indigo-900 mx-auto my-2">
+        Please present your order numbers below for collection. Payment (direct
+        to merchant) only upon collection.
+      </p>
+      <div className="flex w-1/3 mx-auto">
+        <button
+          type="button"
+          className={
+            showHistory
+              ? "mt-3 inline-flex w-full justify-center rounded-l bg-indigo-100 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-indigo-50"
+              : "mt-3 inline-flex w-full justify-center rounded-l bg-indigo-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600"
+          }
+          onClick={() => setShowHistory(false)}
+        >
+          Active orders
+        </button>
+        <button
+          type="button"
+          className={
+            showHistory
+              ? "mt-3 inline-flex w-full justify-center rounded-r bg-indigo-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600"
+              : "mt-3 inline-flex w-full justify-center rounded-r bg-indigo-100 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-indigo-50"
+          }
+          onClick={() => setShowHistory(true)}
+        >
+          Order history
+        </button>
+      </div>
       <div className="w-full">
-        {orders.map((order, idx) => {
-          return <OrderItem order={order} key={idx} />;
-        })}
+        {showHistory
+          ? pastOrders.map((order, idx) => {
+              return <PastOrderItem order={order} key={idx} />;
+            })
+          : activeOrders.map((order, idx) => {
+              return <ActiveOrderItem order={order} key={idx} />;
+            })}
       </div>
     </div>
   );
