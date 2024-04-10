6b0b970762abd65237e194a82f02b331bbc77414
kennethquek <kennethqzw@gmail.com>
2024-04-10 23:25:55 +0800

    fix leaftlet markers' broken image

--- frontend/project3/src/components/DisplayMap.jsx
+++ frontend/project3/src/components/DisplayMap.jsx
@@ -10,6 +10,20 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import styles from "./styles/DisplayMap.module.css";
+import L from "leaflet";
+
+// Import images
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const DisplayMap = (props) => {
   const [screenDimensions, setScreenDimensions] = useState({
