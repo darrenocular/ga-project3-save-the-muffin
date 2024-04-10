04ddca5026f99856d97e5a94f9f4d1d03fa40903
kennethquek <kennethqzw@gmail.com>
2024-04-10 14:16:44 +0800

    style error modal

--- frontend/project3/src/components/ErrorModal.jsx
+++ frontend/project3/src/components/ErrorModal.jsx
@@ -12,14 +12,19 @@ const Overlay = (props) => {
         className={`${styles.board} ${styles.modal}`}
         onClick={(event) => event.stopPropagation()}
       >
-        <header className={styles.errorHeader}>
-          <h2>{props.title}Error</h2>
+        <header className="bg-rose-600 p-4 rounded-t-lg text-white font-semibold">
+          <h2>{props.title || "Error"}</h2>
         </header>
-        <div className="p-3 overflow-y-auto min-h-16 text-gray-900">
-          {props.content}
+        <div className="p-3 overflow-y-auto min-h-16 text-gray-900 leading-6">
+          {props.content && props.content.charAt(-1) !== "."
+            ? props.content + "."
+            : props.content}
         </div>
         <footer className={styles.actions}>
-          <button onClick={props.okayClick} className={``}>
+          <button
+            onClick={props.okayClick}
+            className="inline-flex justify-center rounded-md bg-green-600 ml-2 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500"
+          >
             Cancel
           </button>
         </footer>
